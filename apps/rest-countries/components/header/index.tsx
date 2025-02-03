ecd1384b5cfee11f231d699a7b39998c5de71e3a
developneoguri <developneoguri@gmail.com>
2025-02-04 02:16:38 +0900

    feat: 헤더 텍스트 스타일 추가

--- apps/rest-countries/components/header/index.tsx
+++ apps/rest-countries/components/header/index.tsx
@@ -3,7 +3,7 @@ import { ChangeModeButton } from "./components/change-mode-button";
 export function Header() {
   return (
     <header className="w-full h-20 flex justify-between text-lg px-20 py-7 bg-white fixed top-0 right-0 z-20 justify-between drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
-      <h1>Where in the world?</h1>
+      <h1 className="font-extrabold text-2xl">Where in the world?</h1>
       <ChangeModeButton />
     </header>
   );

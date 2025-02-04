62e900321377f28e320517bef7ee37feb44db944
developneoguri <developneoguri@gmail.com>
2025-02-04 23:33:18 +0900

    feat: 검색창 배치치

--- apps/rest-countries/app/page.tsx
+++ apps/rest-countries/app/page.tsx
@@ -1,14 +1,16 @@
 import { RegionCard } from "@/components/region-card";
+import { SearchBar } from "@/components/search-bar";
 import { Country } from "@/type/type";
 import { Fragment } from "react";
 
 export default async function Home() {
   try {
-    const responseData = await fetch(`http://localhost:3000/api/list`);
+    const responseData = await fetch(`http://localhost:3001/api/list`);
     const countries: Country[] = await responseData.json();
 
     return (
       <Fragment>
+        <SearchBar />
         <div className="grid grid-cols-4 gap-[76px]">
           {countries.map((country) => (
             <RegionCard

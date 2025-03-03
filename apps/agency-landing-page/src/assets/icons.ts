676a206e440a6541f1a440337cfcf27ee8db0674
drunken_neoguri <developneoguri@gmail.com>
2025-03-03 20:21:51 +0900

    add & install: svg 아이콘을 컴포넌트화할 vite plugin 설치 및 설정 적용

---
+++ apps/agency-landing-page/src/assets/icons.ts
@@ -0,0 +1,17 @@
+import ArrowDownIcon from "./icon-arrow-down.svg?react";
+import TwitterIcon from "./icon-twitter.svg?react";
+import FacebookIcon from "./icon-facebook.svg?react";
+import InstagramIcon from "./icon-instagram.svg?react";
+import PinterestIcon from "./icon-pinterest.svg?react";
+import HamburgerIcon from "./icon-hamburger.svg?react";
+import Logo from "./logo.svg?react";
+
+export {
+  ArrowDownIcon,
+  TwitterIcon,
+  FacebookIcon,
+  InstagramIcon,
+  PinterestIcon,
+  HamburgerIcon,
+  Logo,
+};
